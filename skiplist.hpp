d58b5ce7c27761897a08c4516e693cf64a8382b8
stmatengss <805522925@qq.com>
2017-09-20 21:57:37 +0800

    xxx

--- skiplist.hpp
+++ skiplist.hpp
@@ -23,8 +23,9 @@ namespace SkipList {
 		// int max_skiplist_level; // Drop this.
 		std::random_device gen_random;
 		SkipListNode *header;
-		SkipListNode *tail;
+		// SkipListNode *tail; // Useless
 		int skiplist_level;
+		int length;
 
 		SkipList(): skiplist_level(1) {
 
@@ -45,8 +46,10 @@ namespace SkipList {
 			SkipListNode *node;
 			SkipListNode *update[max_skiplist_level];
 			node = header;
-			for (int i = skiplist_level; i >= 0; i -- ) {
-				while (node->next_list[i] && node->next_list[i] < num) {
+			
+			// Find matching position
+			for (int i = skiplist_level - 1; i >= 0; i -- ) {
+				while (node->next_list[i] && node->next_list[i]->value < num) {
 					node = node->next_list[i];
 				}
 				update[i] = node;
@@ -55,23 +58,70 @@ namespace SkipList {
 			SkipListNode *new_node = new SkipListNode(num, level);
 
 			if (level > skiplist_level) {
-				for (i = skiplist_level; i < level; i ++ ) {
-					
+				for (int i = skiplist_level; i < level; i ++ ) {
+					update[i] = header;
 				}
 			}
 
+			// Insert new Node to Skip List
+			for (int i = 0; i < level; i ++ ) {
+				new_node->next_list[i] = update[i]->next_list[i];
+				update[i]->next_list[i] = new_node;
+			}
+
+			length ++;
 			return true;
 		}
 
 		bool drop(double num) {
-			return true;
+			SkipListNode *node = _search(num);
+
+			if (node == NULL || node->next_list[0] == NULL) {
+				return false;
+			}
+
+			if (node->next_list[0]->value == num) {
+				length --;
+				SkipListNode *update[max_skiplist_level];
+				for (int i = 0; i < update; i ++ ) {
+					update[in] = ode->next_list[i];
+				}
+				return true;
+			}
+
+			return false;
 		}
 
 		bool search(double num) {
-			return true;
+			SkipListNode *node = _search(num);
+
+			if (node == NULL || node->next_list[0] == NULL) {
+				return false;
+			}
+
+			if (node->next_list[0]->value == num) {
+				return true;
+			}
+
+			return false;
 		}
 
+		
+
 	private:
+
+		SkipListNode *node _search(double num) {
+			SkipListNode *node = header;
+
+			for (int i = skiplist_level - 1; i >= 0; i -- ) {
+				while (node->next_list[i] && node->next_list[i]->value < num) {
+					node = node->next_list[i];
+				}
+			}
+
+			return node;
+		}
+
 		int random_level() {
 			int num = 1;
 			while (gen_random() % 128 < 64) {
